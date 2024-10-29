6a4afca9ade6609713ab950756305372d13792c7
yuke <96366114+y-anim16@users.noreply.github.com>
2024-10-29 10:39:07 +0900

    update ボーン生成処理実装

--- Addon/CarRigGenerator.py
+++ Addon/CarRigGenerator.py
@@ -1,4 +1,5 @@
 import bpy
+import mathutils
 
 bl_info = {
     "name": "CarRigGenerator",
@@ -28,10 +29,85 @@ class CarRigGenerator(bpy.types.Operator):
         car_parts = context.scene.car_parts_parameters
 
         if car_parts.car_body and car_parts.front_wheel and car_parts.back_wheel:
-            print("Executed")
+            self.create_armature(car_parts)
         else:
             print("No object selected")
         return {'FINISHED'}
+    
+    def create_armature(self, car_parts):
+        bpy.ops.object.armature_add(enter_editmode=True, align='WORLD', location=(0,0,0), scale=(1, 1, 1))
+
+        new_armature = bpy.context.active_object
+        armature_data = new_armature.data
+        armature_data.name = 'ArmatureData'
+
+        primary_bone = bpy.context.active_bone
+        primary_bone.name = 'Primary'
+
+        root_bone = self.add_bone(
+            armature_data, 
+            "Root", 
+            primary_bone.head,
+            primary_bone.tail,
+            primary_bone.roll,
+            primary_bone
+        )
+        
+        front_wheel_obj = bpy.data.objects.get(car_parts.front_wheel)
+        front_wheel_center = self.get_center_coords(front_wheel_obj)
+        half_wheel_thickness = mathutils.Vector((front_wheel_obj.dimensions.x / 2, 0, 0))
+        # TODO rename
+        front_wheel_bone = self.add_bone(
+            armature_data,
+            "FrontWheel_L",
+            front_wheel_center - half_wheel_thickness,
+            front_wheel_center,
+            primary_bone.roll,
+            root_bone            
+        )
+
+        front_wheel_sub_bone = self.add_bone(
+            armature_data,
+            "FrontWheelSub_L",
+            front_wheel_center,
+            front_wheel_center + half_wheel_thickness,
+            primary_bone.roll,
+            front_wheel_bone
+        )
+
+        back_wheel_obj = bpy.data.objects.get(car_parts.back_wheel)
+        back_wheel_center = self.get_center_coords(back_wheel_obj)
+        back_wheel_bone = self.add_bone(
+            armature_data,
+            "BackWheel_L",
+            back_wheel_center,
+            back_wheel_center + mathutils.Vector((back_wheel_obj.dimensions.x / 2, 0, 0)),
+            primary_bone.roll,
+            root_bone
+        )
+
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+
+    def add_bone(self, armature_data, bone_name, head = None, tail = None, roll = None, parent = None):
+        new_bone = armature_data.edit_bones.new(name=bone_name)
+
+        if head:
+            new_bone.head = head
+        if tail:
+            new_bone.tail = tail
+        if roll:
+            new_bone.roll = roll
+        if parent:
+            new_bone.parent = parent
+
+        return new_bone
+    
+    def get_center_coords(self, obj):
+        bb = obj.bound_box
+        bb_world_coords = [obj.matrix_world @ mathutils.Vector(corner) for corner in bb]
+        return sum(bb_world_coords, mathutils.Vector()) / 8
+
 
 class CarRigGeneratorUi(bpy.types.Panel):
     bl_idname = "car_rig_generator_ui"
